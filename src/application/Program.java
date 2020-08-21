6ab4b6497dee94949e74dda7a169bee53c84568d
Nicholas Bendel <ndacosta64@gmail.com>
2020-08-21 16:57:37 -0300

    delete department

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,16 +2,13 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
-import model.entities.Department;
 
 public class Program {
 	public static void main(String[] args) {
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-		System.out.println("Teste update departamento");
-		Department department = new Department(7,"Update confirmado");
-		departmentDao.update(department);
-		System.out.println(department);
+		System.out.println("Teste delete departamento");
+		departmentDao.deleteById(7);
 		
 	}
 }
