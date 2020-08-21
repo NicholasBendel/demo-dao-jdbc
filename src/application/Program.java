ecfe57c9da914c6f6ab0d78e82da034533bab2eb
Nicholas Bendel <ndacosta64@gmail.com>
2020-08-21 16:54:55 -0300

    update department

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,10 +8,10 @@ public class Program {
 	public static void main(String[] args) {
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-		System.out.println("Teste inserir departamento");
-		Department department = new Department(null,"Teste");
-		departmentDao.insert(department);
-		System.out.println(department.getId());
+		System.out.println("Teste update departamento");
+		Department department = new Department(7,"Update confirmado");
+		departmentDao.update(department);
+		System.out.println(department);
 		
 	}
 }
