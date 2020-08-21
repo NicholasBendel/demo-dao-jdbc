42b905bea816941db203ddf96917f4bf0c24adc7
Nicholas Bendel <ndacosta64@gmail.com>
2020-08-21 14:14:27 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import modelo.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department d = new Department(1,"Books");
+		
+		System.out.println(d);
+	}
+
+}
